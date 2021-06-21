3c7b14e1066fffbf1e732a53b3b84fd2c3346bb6
hongphuc9595 <45747789+hongphuc9595@users.noreply.github.com>
2021-06-21 11:32:20 +0700

    Delete Storage.ps1

--- Storage.ps1
+++
@@ -1,9 +0,0 @@
-$connectTestResult = Test-NetConnection -ComputerName accountsharefilenhphuc.file.core.windows.net -Port 445
-if ($connectTestResult.TcpTestSucceeded) {
-    # Save the password so the drive will persist on reboot
-    cmd.exe /C "cmdkey /add:`"accountsharefilenhphuc.file.core.windows.net`" /user:`"Azure\accountsharefilenhphuc`" /pass:`"WRDu06VWMI7ppQQGHVP80l5HxBs4SHPZbgUyz6j5sVo+gxXBA8cXwTti/SRr3mLHprIeKNv3qBDRvlxQM858Bw==`""
-    # Mount the drive
-    New-PSDrive -Name Z -PSProvider FileSystem -Root "\\accountsharefilenhphuc.file.core.windows.net\share" -Persist
-} else {
-    Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
-}
