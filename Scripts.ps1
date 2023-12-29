026a683fbbcad6ed7341c6e02bb75aedf15212fb
hongphuc9595 <45747789+hongphuc9595@users.noreply.github.com>
2023-12-29 15:26:16 +0700

    Update Scripts.ps1

--- Scripts.ps1
+++ Scripts.ps1
@@ -30,15 +30,29 @@ function Reset-IDM {
     iex (irm is.gd/idm_reset)
 }
 
+# Function to Install Basic Applications using Winget
+function Install-BasicAppsWithWinget {
+    winget install --id=Google.Chrome -e
+    winget install --id=Mozilla.Firefox -e
+    winget install --id=TeamViewer.TeamViewer -e
+    winget install --id=UniKey.UniKey -e
+    winget install --id=RARLab.WinRAR -e
+    winget install --id=Adobe.Acrobat.Reader.64-bit -e
+    winget install --id=Microsoft.Teams -e
+    winget install --id=VNGCorp.Zalo -e
+    winget install --id=Viber.Viber -e
+}
+
 # Create a menu for user selection
 do {
     Write-Host "======== Menu Options ========"
     Write-Host "1. Install Chocolatey"
-    Write-Host "2. Install Basic Applications"
+    Write-Host "2. Install Basic Applications (Chocolatey)"
     Write-Host "3. Install Microsoft 365 Apps for Business"
     Write-Host "4. Install Office 2021"
     Write-Host "5. Activate Office"
     Write-Host "6. Reset IDM (Internet Download Manager)"
+    Write-Host "7. Install Basic Applications (Winget)"
     Write-Host "Q. Exit the program"
     $choice = Read-Host "Enter your choice"
     switch ($choice) {
@@ -60,6 +74,9 @@ do {
         '6' {
             Reset-IDM
         }
+        '7' {
+            Install-BasicAppsWithWinget
+        }
         'Q' {
             # Exit the program
             break
@@ -69,4 +86,4 @@ do {
             Write-Host "Invalid choice. Please try again."
         }
     }
-} while ($choice -ne 'Q' )
+} while ($choice -ne 'Q')
