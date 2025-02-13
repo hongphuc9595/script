52eeb004e8fc0a396697f1c4cf9f716f731dc54f
hongphuc9595 <45747789+hongphuc9595@users.noreply.github.com>
2025-02-13 13:36:16 +0700

    Delete exchange.ps1

--- exchange.ps1
+++
@@ -1,6 +0,0 @@
-﻿Set-ExecutionPolicy RemoteSigned
-$LiveCred = Get-Credential
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $LiveCred -Authentication Basic -AllowRedirection
-Import-PSSession $Session
-
-
